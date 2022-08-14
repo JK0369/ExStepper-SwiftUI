71af87797fd88a26764e7eb8886f4eca00b33395
jake <palatable7@naver.com>
2022-08-14 20:28:51 +0900

    Initial Commit

---
+++ ExStepper/ExStepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExStepperApp.swift
+//  ExStepper
+//
+//  Created by 김종권 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct ExStepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
